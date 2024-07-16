1fdc6b3cfbcc1d454610ee684deeb57e18704adc
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-07-16 11:58:07 +0530

    simpleInterest&GreaterElement

---
+++ GreaterAorB.java
@@ -0,0 +1,12 @@
+public class GreaterAorB {
+    public static void main(String[] args) {
+        int a = 85;
+        int b = 90;
+        if (a > b) {
+            System.out.print("A is greater element");
+        }
+        else {
+            System.out.print("B is greater element");
+        }
+    }
+}
---
+++ SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int p = 10000;
+        int r = 10;
+        int t = 2;
+        int si = (p * r * t) / 100;
+        System.out.print(si);
+    }
+}
