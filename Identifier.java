ba9237d6b9679200a9d4297e8e1c6a0b69ee2f12
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-07-17 10:19:26 +0530

    commit2

---
+++ Identifier.java
@@ -0,0 +1,9 @@
+public class Identifier {
+    public static void main(String[] args) {
+        //All identifiers can begin with a letter, a currency symbol or an underscore (_)
+        int a_letter = 10;
+        int $dollar = 5;
+        int _underscore = 200;
+        System.out.println(a_letter + " " + $dollar + " " + _underscore);
+    }
+}
---
+++ Patterns.java
@@ -0,0 +1,57 @@
+// import java.util.scanner;
+
+public class Patterns {
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 10;
+        int i = 0;
+        int k = 0;
+
+        /* 
+        
+        * * * *
+        * * * *
+        * * * *
+        * * * *
+        
+         */
+        // using for loop
+        for (i = 0; i < n;i++) {
+            for (k = 0 ; k<n;k++) {
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+
+        // using while loop
+        while (i < m) {
+            int j = 0;
+            while (j < m) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        /*
+        
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+        
+         */
+        int o = 5;
+        int a = 0;
+        while (a < o) {
+            int l = 0;
+            while (l <= a) {  
+                System.out.print("* ");
+                l++;
+            }
+            a++;
+            System.out.println();
+        }
+    }
+}
