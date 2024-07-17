458061c413e910b4c71b4f57cfd406f4e2a4a8d6
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-07-17 09:23:46 +0530

    comments in java

---
+++ comment.java
@@ -0,0 +1,19 @@
+/** this is a document comment */
+
+//this is a single line comment
+
+/*
+ this is multiline comment
+ you can write multiply lines in this
+
+ ~comments are used for wrting notes or anything by which it will be easier to identify the code for different things
+ ~you can add comment anywhere in the code 
+
+ */
+
+public class comment {
+    public static /*comments can be added anywhere in the code */ void main(String[] args) {
+        int a = 5; //but if you write it in specific place where it needed then it will be beneficial
+        System.out.print(a);
+    }
+ }
