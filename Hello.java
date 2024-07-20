65bb2248ea287b0135ebb5064f800d77bf39fed7
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-07-20 14:13:40 +0530

    commit

--- Hello.java
+++ Hello.java
@@ -16,14 +16,6 @@ public class Hello {
     }
 }
 
+//Time Complexity: O(1)
 
-/*public class Hello {
-    public static void main(String[] args) {
-        int i = 0;
-        while (i < 10) {
-            System.out.println("hello");
-            i++;
-        }
-    }
-}
-*/
+// Space Complexity: O(1)
