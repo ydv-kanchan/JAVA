2f6a61a6c51e3be4439ff379b8eb73ae0b2739d8
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-07-23 09:59:09 +0530

    octalorint

---
+++ OctalOrInt.java
@@ -0,0 +1,19 @@
+public class OctalOrInt {
+    public static void main(String[] args) {
+        int a = 054; //every number staring with 0 and having all digits less than 8 are considered as octal number
+        //but it will give error if we will write 0  in front of any other number with any digit more than 0
+        // int b = 058; //out of range error
+        System.err.println(a);
+        // System.out.println(b);
+
+        // range of byte
+        byte i = 122; //as 122 is ealisy converter to byte
+
+        // byte j = 140; //error can not convert int to byte, so need to do explicit type casting
+        byte j = (byte) 140; // will give -116
+        byte k = (byte) 300; 
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
